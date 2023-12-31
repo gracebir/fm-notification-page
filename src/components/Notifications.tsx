bb077dfa653afd4d0ffa9e0bf7cfb2f62fc36f2d
gracebir <grace.devolop@gmail.com>
2023-12-31 20:59:57 +0200

    print message message notification

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -1,10 +1,12 @@
 /** @format */
 "use client";
+import { datas } from "@/data";
 import React from "react";
+import Message from "./Message";
 
 const Notifications = () => {
     return (
-        <div className='max-w-4xl bg-white lg:rounded-lg rounded-none lg:shadow-md shadow-none mx-auto py-10 px-4 lg:px-8'>
+        <div className='max-w-3xl bg-white flex flex-col gap-8 lg:rounded-lg rounded-none lg:shadow-md shadow-none mx-auto py-10 px-4 lg:px-8'>
             <div className='flex justify-between items-center'>
                 <div className='flex gap-4 items-center'>
                     <h3 className='font-extrabold lg:text-2xl text-lg'>
@@ -17,6 +19,22 @@ const Notifications = () => {
                 <span className='text-sm lg:text-base cursor-pointer'>
                     Mark all as read
                 </span>
+            </div>
+            <div className='flex flex-col gap-4'>
+                {datas.map((item) => (
+                    <Message
+                        replyText={item.replyText!}
+                        isRead={item.isRead}
+                        messageText={item.messageText}
+                        groupName={item.groupName!}
+                        mediaSent={item.mediaSent!}
+                        boldedtext={item.boldedtext!}
+                        key={item.id}
+                        timePosted={item.timePosted}
+                        username={item.username}
+                        avatar={item.avatar}
+                    />
+                ))}
             </div>
         </div>
     );
